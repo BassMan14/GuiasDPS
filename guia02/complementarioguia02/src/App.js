5c63ac3861177c2853a1442f28ae83bdd5398a88
BassMan14 <bryanarmandoss@hotmail.com>
2021-08-17 12:46:57 -0600

    comp

---
+++ guia02/complementarioguia02/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Form from './components/Form';
+
+const App = () => {
+  return(
+    <div className="App">
+      <div className="App-content">
+        <p>
+          Todo list
+        </p>
+        <Form /> 
+      </div>
+    </div>
+  );
+}
+
+export default App;
