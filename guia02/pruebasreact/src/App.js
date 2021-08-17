528ad674f3f6f560af62207ffbda447aa0d48a56
BassMan14 <bryanarmandoss@hotmail.com>
2021-08-17 12:01:13 -0600

    guia 02

---
+++ guia02/pruebasreact/src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+import Form from './components/Form';
+
+const App = () => {
+  return(
+    <div className="App">
+      <div className="App-content">
+        <p>
+          Todo list
+        </p>
+        <Form /> 
+      </div>
+    </div>
+  );
+}
+
+export default App;
